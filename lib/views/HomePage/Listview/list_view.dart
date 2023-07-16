3724f0e30d2076b2ebd755125b1520bc7fe8725b
pulkitjoshi <pulkit.joshi@leadsquared.com>
2023-07-17 00:36:17 +0530

    pet adoption changes

--- lib/views/HomePage/Listview/list_view.dart
+++ lib/views/HomePage/Listview/list_view.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pet_adoption/bloc/petadoptionbloc/petadoptionbloc.dart';
-import 'package:pet_adoption/bloc/petadoptionbloc/petadoptionevents.dart';
 import 'package:pet_adoption/models/petmodel.dart';
 import 'package:pet_adoption/views/detailspage/details_page.dart';
 import 'package:pet_adoption/views/homepage/card/card.dart';
@@ -19,18 +16,6 @@ class CustomListView extends StatelessWidget {
     return ListView.builder(
       itemCount: pets.length + 1, // +1 for the button
       itemBuilder: (context, index) {
-        if (index == pets.length) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(primary: Theme.of(context).primaryColor),
-              onPressed: () {
-                BlocProvider.of<PetAdoptionBloc>(context).add(FetchAllPetsEvent());
-              },
-              child: const Text('Load More'),
-            ),
-          );
-        }
         final pet = pets[index];
         return GestureDetector(
           onTap: () {
